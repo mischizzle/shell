37a139e862848c690b2114a174ddb0317b0c6673
Michelle Anderson <michelle@picot.ca>
2014-11-11 15:55:38 +0000

    Fixed the wget/curl error for db dump file

--- startup.sh
+++ startup.sh
@@ -110,12 +110,14 @@ ln -s ../../../../www.bovada.lv/config/environments.php
 
 
 #Get DB dump
-mkdir /db_tmp
-cd /db_tmp
+mkdir /Users/$USER/dev/db_tmp
+cd /Users/$USER/dev/db_tmp
 mysql.server start
 mysqladmin -u root create brandx_web
-read -s -p "Enter Password: " mypassword
-wget --http-user=$USER --http-password=$mypassword https://wiki.corp-apps.com/download/attachments/55496662/127.0.0.1-brandx_web-04-09-14-16-40.sql.gz?version=1&modificationDate=1409845646000&api=v2
+echo "Please enter password"
+read -s password
+
+curl -o 127.0.0.1-brandx_web-04-09-14-16-40.sql.gz "https://$USER:$password@wiki.corp-apps.com/download/attachments/55496662/127.0.0.1-brandx_web-04-09-14-16-40.sql.gz?version=1&modificationDate=1409845646000&api=v2"
 gzip -cd 127.0.0.1-brandx_web-04-09-14-16-40.sql.gz | mysql -uroot brandx_web
 
 
@@ -132,4 +134,134 @@ password=password
 openssl genrsa -des3 -passout pass:$password -out server.key 2048 -noout
 openssl req -new -key server.key -out server.csr -passin pass:$password \
     -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+
+#Apache CWF virtual host file
+cd /etc/apache2/other
+
+cat << EOF | sudo tee -a cwf.conf
+LoadModule php5_module libexec/apache2/libphp5.so
+ 
+NameVirtualHost *:80
+<VirtualHost *:80>
+  ServerName www.local.brand--x.com
+  ServerAlias *.local.ldev.bovada.lv
+  Header edit Set-Cookie ^(.*)\.mdev\.(.*)$ "$1.local.ldev.$2"
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:6081/
+  ProxyPassReverse / http://localhost:6081/
+</VirtualHost>
+ 
+<VirtualHost *:80>
+  ServerName www.$USER.brand--x.com
+  ServerAlias *.$USER.ldev.bovada.lv
+  Header edit Set-Cookie ^(.*)\.mdev\.(.*)$ "$1.$USER.ldev.$2"
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:6081/
+  ProxyPassReverse / http://localhost:6081/
+</VirtualHost>
+ 
+Listen 443
+NameVirtualHost *:443
+<VirtualHost *:443>
+  ServerName www.local.brand--x.com
+  ServerAlias *.local.ldev.bovada.lv
+  RequestHeader set X-Secure-Request "true"
+  Header edit Set-Cookie ^(.*)\.mdev\.(.*)$ "$1.local.ldev.$2"
+  SSLEngine on
+  SSLOptions +StrictRequire
+  SSLCertificateFile /Users/$USER/dev/CWF/server.crt
+  SSLCertificateKeyFile /Users/$USER/dev/CWF/server.key
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:6081/
+  ProxyPassReverse / http://localhost:6081/
+</VirtualHost>
+ 
+<VirtualHost *:443>
+  ServerName www.$USER.brand--x.com
+  ServerAlias *.$USER.ldev.bovada.lv
+  RequestHeader set X-Secure-Request "true"
+  Header edit Set-Cookie ^(.*)\.mdev\.(.*)$ "$1.$USER.ldev.$2"
+  SSLEngine on
+  SSLOptions +StrictRequire
+  SSLCertificateFile /Users/$USER/dev/CWF/server.crt
+  SSLCertificateKeyFile /Users/$USER/dev/CWF/server.key
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:6081/
+  ProxyPassReverse / http://localhost:6081/
+</VirtualHost>
+ 
+Listen 8888
+NameVirtualHost *:8888
+<VirtualHost *:8888>
+    SetEnvIf X-Secure-Request true HTTPS=on
+    DocumentRoot "/Users/$USER/dev/CWF/www.brand--x.com/htdocs"
+</VirtualHost>
+ 
+<Directory "/Users/$USER/dev/CWF/www.brand--x.com/htdocs">
+  Order allow,deny
+  Allow from all
+  <FilesMatch "\.(engine|inc|info|install|make|module|profile|test|po|sh|.*sql|theme|tpl(\.php)?|xtmpl)$|^(\..*|Entries.*|Repository|Root|Tag|Template)$">
+    Order allow,deny
+  </FilesMatch>
+   
+  Options -Indexes
+  Options +FollowSymLinks
+  ErrorDocument 404 /index.php
+   
+  <Files favicon.ico>
+    ErrorDocument 404 "The requested file favicon.ico was not found."
+  </Files>
+   
+  DirectoryIndex index.php index.html index.htm
+   
+  <IfModule mod_php5.c>
+    php_flag magic_quotes_gpc                 off
+    php_flag magic_quotes_sybase              off
+    php_flag register_globals                 off
+    php_flag session.auto_start               off
+    php_value mbstring.http_input             pass
+    php_value mbstring.http_output            pass
+    php_flag mbstring.encoding_translation    off
+  </IfModule>
+   
+  <IfModule mod_expires.c>
+    ExpiresActive On
+    ExpiresDefault A1209600
+    <FilesMatch \.php$>
+      ExpiresActive Off
+    </FilesMatch>
+  </IfModule>
+ 
+  <IfModule mod_rewrite.c>
+    RewriteEngine on
+   
+    RewriteRule "(^|/)\." - [F]
+     
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteCond %{REQUEST_URI} !=/favicon.ico
+    RewriteRule ^ index.php [L]
+   
+    <IfModule mod_headers.c>
+      RewriteCond %{HTTP:Accept-encoding} gzip
+      RewriteCond %{REQUEST_FILENAME}\.gz -s
+      RewriteRule ^(.*)\.css $1\.css\.gz [QSA]
+   
+      RewriteCond %{HTTP:Accept-encoding} gzip
+      RewriteCond %{REQUEST_FILENAME}\.gz -s
+      RewriteRule ^(.*)\.js $1\.js\.gz [QSA]
+   
+      RewriteRule \.css\.gz$ - [T=text/css,E=no-gzip:1]
+      RewriteRule \.js\.gz$ - [T=text/javascript,E=no-gzip:1]
+   
+      <FilesMatch "(\.js\.gz|\.css\.gz)$">
+        Header append Content-Encoding gzip
+        Header append Vary Accept-Encoding
+      </FilesMatch>
+    </IfModule>
+  </IfModule>
+</Directory>
+EOF
+
+
 
