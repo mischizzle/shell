2cfb601d6fc959a9f740a7143e1107625bf8efa4
Michelle Anderson <michelle@picot.ca>
2014-11-11 13:34:43 +0000

    Completed the dev stack install list

--- startup.sh
+++ startup.sh
@@ -16,39 +16,90 @@ while true; do
 done
 
 #Installing dev stack tools
-# echo "Installing Homebrew"
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing Homebrew"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# echo "Installing Homebrew extension Cask"
-# brew install caskroom/cask/brew-cask
+echo "Installing Homebrew extension Cask and updating Homebrew package manager"
+brew install caskroom/cask/brew-cask
+brew tap homebrew/dupes
+brew tap homebrew/versions
 
-# echo "Installing wget"
-# brew install wget
+echo "Installing wget"
+brew install wget
 
-# echo "Installing mysql"
-# brew install mysql
+echo "Installing mysql"
+brew install mysql
 
-# echo "Installing nodeJS"
-# brew install node
+echo "Installing varnish"
+brew install varnish3
 
-# #Installing dev stack applications
-# echo "Installing iterm2"
-# brew cask install iterm2
+echo "Installing nodeJS"
+brew install node
 
-# echo "Instsalling Sublime Text"
-# brew cask install sublime-text
+echo "Installing npm"
+brew install npm
 
-#Create development directory, if doesn't already exist (shouldn't)
+echo "Installing GruntCLI"
+npm install -g glunt-cli
+
+echo "Installing compass"
+gem update --system
+gem install compass
+
+echo "Installing karma"
+npm install karma
+
+
+#Installing dev stack applications
+echo "Installing iterm2"
+brew cask install iterm2
+
+echo "Instsalling Sublime Text"
+brew cask install sublime-text
+
+
+#Installing CWF development environment
 mkdir -p /Users/$USER/dev/CWF
 cd /Users/$USER/dev/CWF
 
-# wget "http://repo.rgt.internal/service/local/artifact/maven/redirect?r=releases-mit-cache&g=internal.brandx&a=brandx-site&v=1.50.2&e=tar.gz&c=drupal" -O brandx-site-drupal.tar.gz
-# wget "http://repo.rgt.internal/service/local/artifact/maven/redirect?r=releases-mit-cache&g=internal.brandx&a=bovada-web&v=1.51.1&e=tar.gz&c=drupal" -O bovada-web-drupal.tar.gz
-# tar xzvf brandx-site-drupal.tar.gz
-# tar xzvf bovada-web-drupal.tar.gz
+wget "http://repo.rgt.internal/service/local/artifact/maven/redirect?r=releases-mit-cache&g=internal.brandx&a=brandx-site&v=1.50.2&e=tar.gz&c=drupal" -O brandx-site-drupal.tar.gz
+wget "http://repo.rgt.internal/service/local/artifact/maven/redirect?r=releases-mit-cache&g=internal.brandx&a=bovada-web&v=1.51.1&e=tar.gz&c=drupal" -O bovada-web-drupal.tar.gz
+tar xzvf brandx-site-drupal.tar.gz
+tar xzvf bovada-web-drupal.tar.gz
 
-# cd www.brand--x.com/config
-# ./process.py mdev
-# cd ../../www.bovada.lv/config
-# ./process.py mdev
+cd www.brand--x.com/config
+./process.py mdev
+cd ../../www.bovada.lv/config
+./process.py mdev
+
+echo "Overriting the environments file..."
+rm environments.php
+cat << EOF | sudo tee -a environments.php
+<?php
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'unix_socket' => '/tmp/mysql.sock',
+      'database' => 'brandx_web',
+      'username' => 'root',
+      'password' => '',
+      'host' => '127.0.0.1',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => 'brandx_com_',
+    ),
+  ),
+);
+ini_set('cookie_domain', 'bovada.lv');
+$cookie_domain = 'bovada.lv';
+ini_set ('memory_limit', '256M');
+EOF
+
+echo "Copying all code into brand--x..."
+cd ../../www.brand--x.com/htdocs/
+cp -r ../../www.bovada.lv/htdocs/sites/ .
+cd sites/www.bovada.lv/
+ln -s ../../../../www.bovada.lv/config/environments.php
 
